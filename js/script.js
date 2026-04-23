bb4cc5cd62f5eb91a37335e035a676188e2d1165
Santechweb <gnsarq@gmail.com>
2026-04-23 11:00:58 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,3 @@
+// Arquivo JavaScript do site
+// Por enquanto ele está vazio porque ainda não colocamos interações.
+// Estamos criando ele agora apenas para evitar erro caso o index.html tente carregá-lo.
